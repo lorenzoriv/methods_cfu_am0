c2094b8b9dbaeb426fcec356d0bd10b655a83320
lorenzo rivera <lorenzo23rivera@gmail.com>
2023-02-24 06:43:41 -0800

    Add methods code and self eval.

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -7,22 +7,22 @@
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
 "Hello World".downcase
-
+#it is telling ruby to take the string and lowercase all of the charaters in it.
 
 "Hello World".include?("Hello")
-
+#this is taking the string and asking if it conatins the string hello,which it does so it would return as true.
 
 "Hello World".end_with?("Hello")
-
+#kind of like include it is asking if it has the string hello in it but this time if the string ends with the string hello which it does not so it would return as false.
 
 "Hello World".end_with?("rld")
-
+#so it is asking if the string hello world ends with the string rld in which it does so it would return as true.
 
 12.even?
-
+#this is asking if the integer is even or not in which it is so it would return as true.
 
 18.next
-
+#this method is telling the Integer to increase its value by 1 so it would return as 19.
 
 
 # SECTION 2: Calling methods on variables assigned to strings.
@@ -38,7 +38,11 @@
 # The puts command prints the return value of the start_with? method (true) to the console.
 first_name = "Jeff"
 puts first_name.start_with?("J")
-
+# the size method is called on the variable name,which stores the string "lorenzo".
+# the size method returns an integer of the amount of charaters in the string.
+# This variable has 7 charaters in the string attached to it so the console would return a vaule of 7.
+name = "lorenzo"
+p name.size
 
 
 # SECTION 3: Calling methods on variables assigned to integers.
@@ -47,10 +51,26 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
-
+# the odd? method asks the variable if the integer in said variable is odd or even.
+# in this case 135 is an odd integer so it would return a value of true for the variable weight.
+# the div method divides the integer by the integer placed in the () or by itself if the word numeric is placed in the ().
+# in this case I put div(2) so it will divide age by 2,which would return a integer of 9.
+age = 18
+weight = 135
+p weight.odd?
+p age.div(2)
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# the length method in an array returns an integer of the amount of elements in said array.
+# in this example it returns an integer of 4 due to the four strings in the array.
+# the minmax method returns the elements in the array with the highest and lowest values.
+# in this case it will return 13 and 16 due to those having the minimum and maximum the values go to in the array.
+class_names = ["john","bill","sam","leroy"]
+class_age = [14,13,16,15]
+p class_names.length
+p class_age.minmax
--- final_practice.rb
+++ final_practice.rb
@@ -5,20 +5,36 @@
 
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
-
+def greeting
+    puts "Hello user!"
+  end
+  greeting
 # What is the return value of your method?
 # How many arguments did you pass your method?
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
-
+def custom_greeting(name)
+    puts "Hello #{name}!"
+  end
+  custom_greeting("lorenzo")
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
-
+def greet_person
+    puts "Hello, there. nice to meet you!  What is your first name? "
+    first = gets.chomp
+    puts "and also what is your middle name? "
+    middle = gets.chomp
+    puts "And finally,What is your last name?"
+    last = gets.chomp
+    puts 'It is wonderful to meet you, ' + first  + ' ' + middle + ' ' + last + '.' 
+      puts "my name is computer"
+    end
+    greet_person
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
@@ -35,14 +51,29 @@
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
-check_stock(4, "Coffee");
+#check_stock(4, "Coffee");
 # => "Coffee is stocked"
 
-check_stock(3, "Tortillas");
+#check_stock(3, "Tortillas");
 # => "Tortillas - running LOW"
 
-check_stock(0, "Cheese");
+#check_stock(0, "Cheese");
 # => "Cheese - OUT of stock!"
 
-check_stock(1, "Salsa");
+#check_stock(1, "Salsa");
 # => "Salsa - running LOW"
+
+def check_stock(amount = 0,food)
+    @amount = 0
+    @food = food
+    if
+      amount == 0
+      p "#{food} - out of stock!"
+    elsif
+      amount <=3
+      p "#{food} - running LOW"
+    else 
+      p "#{food}is fully stocked"
+    end 
+  end 
+  check_stock(0,"coffee")
